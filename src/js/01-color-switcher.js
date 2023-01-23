42c22d9c5ab3a4ddbee87ffb394221031e32013c
Gotsman Roman <gotsmanroman@gmail.com>
2023-01-24 00:20:58 +0200

    Додано Завдання 1 - Перемикач кольорів

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let intervalId = null;
+const INTERVAL_TIMER = 1000;
 
+const colorResult = function setRandomBgColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  intervalId = setInterval(colorResult, INTERVAL_TIMER);
+});
+refs.stopBtn.addEventListener('click', () => {
+  refs.startBtn.removeAttribute('disabled');
+  clearInterval(intervalId);
+});
